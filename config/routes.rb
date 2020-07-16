54b6da3554c2212623b23004f6410c5dedb02d80
田中 開 <tanakahal@tanakanoMacBook-Pro.local>
2020-07-16 14:51:11 +0900

    [fix]Bookers_app

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+
+    root "home#top"
+
+    get 'books/' => 'books#index'
+
+    get 'books/new'
+
+    post 'books' => 'books#create'
+
+    get 'books/show'
+
+    get 'books/:id' => 'books#show', as: 'book'
+
+    get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+    patch 'books/:id' => 'books#update', as: 'update_book'
+
+    delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
+   end
